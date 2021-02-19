ad5743bdb2510ff9386ee6b6f476303541913e0f
Eshya <achmadayas@gmail.com>
2021-02-19 11:23:56 +0700

    Update Kedua

--- BOILER_PLC/BOILER_PLC.ino
+++ BOILER_PLC/BOILER_PLC.ino
@@ -5,10 +5,12 @@
 #define FIREBASE_HOST "https://finalprojectar-28391.firebaseio.com/"///////// Addres Realtime Database
 #define FIREBASE_AUTH "lEcynnm4wrMbl0VbTCpmI2vGuJAKL5Lo2bRvL5Ik"////////// 
 WiFiClient client;           // initialize the library instance:
-//#define WIFI_SSID "asekalebor"
-//#define WIFI_PASSWORD "mugiwaraboy"
-#define WIFI_SSID "Infinix"
-#define WIFI_PASSWORD "wanipiro"
+#define WIFI_SSID "asekalebor"
+#define WIFI_PASSWORD "12345678"
+//#define WIFI_SSID "DESKTOP-LABKOM"
+//#define WIFI_PASSWORD "12345678"
+//#define WIFI_SSID "Infinix"
+//#define WIFI_PASSWORD "wanipiro"
 FirebaseData firebaseData;
 #define E 0
 #define LL 1
@@ -18,6 +20,7 @@ FirebaseData firebaseData;
 #define PIN_INTERNET D7
 int MikroPLC_addres[12];
 void defaultDataMikroPLC(){
+
   MikroPLC_addres[0] = 0;
   MikroPLC_addres[1] = 0;
   MikroPLC_addres[2] = 0;
@@ -30,6 +33,7 @@ void defaultDataMikroPLC(){
   MikroPLC_addres[9] = 0;
   MikroPLC_addres[10] = 0;
   MikroPLC_addres[11] = 0;
+  
 }
 void setup() {
   // put your setup code here, to run once:
@@ -60,9 +64,11 @@ void setup() {
   Serial.println();
 }
 int i = 1;
-
+long timeSend;
+long timeLooping;
 void loop() {
   // put your main code here, to run repeatedly:
+  timeLooping = millis();
   while (client.available()) {
     char c = client.read();
     Serial.print(c);
@@ -78,18 +84,20 @@ void loop() {
   MikroPLC_addres[7] = ModbusRTUServer.coilRead(0x03);
   MikroPLC_addres[8] = ModbusRTUServer.coilRead(0x04); // internet
   MikroPLC_addres[9] = ModbusRTUServer.coilRead(0x05); // pump
+  MikroPLC_addres[10] = ModbusRTUServer.coilRead(0x06); // Auto
+  
 //  Serial.print(MikroPLC_addres[0]);
 //  Serial.print(MikroPLC_addres[1]);
 //  Serial.print(MikroPLC_addres[2]);
 //  Serial.print(MikroPLC_addres[3]);
-  Serial.print("::");
-  Serial.print(MikroPLC_addres[4]);
-  Serial.print(MikroPLC_addres[5]);
-  Serial.print(MikroPLC_addres[6]);
-  Serial.print(MikroPLC_addres[7]);
-  Serial.print(MikroPLC_addres[9]);
-  Serial.print("::");
-  Serial.println();
+//  Serial.print("::");
+//  Serial.print(MikroPLC_addres[4]);
+//  Serial.print(MikroPLC_addres[5]);
+//  Serial.print(MikroPLC_addres[6]);
+//  Serial.print(MikroPLC_addres[7]);
+//  Serial.print(MikroPLC_addres[9]);
+//  Serial.print("::");
+//  Serial.println();
   float LEVEL_SEND;
   if(MikroPLC_addres[4] == 1){
     LEVEL_SEND = LL;
@@ -114,17 +122,51 @@ void loop() {
 //  }
   //ModbusRTUServer.coilWrite(0x05,1);
   
-  kirimFirebaseFloat("Sensor/Level",LEVEL_SEND);
-  if(MikroPLC_addres[9]){
-    kirimFirebaseString("Output/Pump","ON");
+  if(millis() - timeSend > 1000){
+    long timeKirim = millis();
+    kirimFirebaseFloat("Sensor/Level",LEVEL_SEND);
+    kirimFirebaseFloat("Safety/Koneksi",random(300));
+      Serial.print("Level : ");
+      Serial.print("3");
+      Serial.println();
+    if(MikroPLC_addres[9]){
+      kirimFirebaseString("Output/Pump","ON");
+      Serial.print("Pompa Boiler: ");
+      Serial.print("ON");
+      Serial.println();
+    }
+    else{
+      kirimFirebaseString("Output/Pump","OFF");
+      Serial.print("Pompa Boiler: ");
+      Serial.print("OFF");
+      Serial.println();
+    }
+  
+    if(MikroPLC_addres[10]){
+      kirimFirebaseString("Output/Auto","ON");
+      Serial.print("Mode Auto: ");
+      Serial.print("ON");
+      Serial.println();
+    }
+    else{
+      kirimFirebaseString("Output/Auto","OFF");
+      Serial.print("Mode Auto: ");
+      Serial.print("OFF");
+      Serial.println();
+      
+    }
+      Serial.print("Waktu Kirim Database : ");
+      Serial.print(millis()-timeKirim);
+      Serial.println();
+    timeSend = millis();
   }
-  else{
-    kirimFirebaseString("Output/Pump","OFF");
-  }
+  
+  
+  
  //+
-  cek_Output(i);
-  i++;
-  if(i>3)i=1;
+  cek_Output(i); // read from firebase, not yet using it so you can comment it
+//  i++;
+//  if(i>3)i=1;
   uint16_t dt_bit;
   if(WiFi.status() == WL_CONNECTED){
     dt_bit = 0x01;
@@ -143,6 +185,9 @@ void loop() {
 //      Serial.println(" Not Sended");
 //    }
   }
-  //delay(100);
+      Serial.print("Waktu Looping: ");
+      Serial.print(millis()-timeLooping);
+      Serial.println();
+  delay(100);
   
 }
